2659e2414d21867f0da0200179af448d36a66b76
Courtney McBeth <clm325@cornell.edu>
2022-09-10 19:45:30 -0500

    debugging

--- js/player_checker.js
+++ js/player_checker.js
@@ -1,8 +1,3 @@
-// Requiring fs module in which
-// readFile function is defined.
-const fs = require('fs')
-
-var found = false;
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const fname = urlParams.getAll('fname');
@@ -11,22 +6,18 @@ console.log(urlParams.getAll('fname'));
 console.log(urlParams.getAll('lname'));
 const fullname = fname[0] + " " + lname[0];
 
-// Reading data in utf-8 format
-// which is a type of character set.
-// Instead of 'utf-8' it can be 
-// other character set also like 'ascii'
-fs.readFile('authorized.txt', 'utf-8', (err, data) => {
-  if (err) throw err;
+fetch('authorized.txt')
+  .then(response => response.text())
+  .then(text => setDisplay(text));
 
-  // Converting Raw Buffer to text
-  // data using tostring function.
-  console.log(data);
-  found = data.toString().includes(fullname)
-})
+function setDisplay(text) {
+  console.log(text);
 
-if (!found) {
-  document.getElementById("auth_label").innerText = "Sorry, you are not authorized to join...";
-} else {
-  document.getElementById("auth_label").innerText = "Welcome " + fname + ", enter your choices below: ";
-  document.getElementById("bracket").style.visibility = "visible";
+  const found = text.includes(fullname);
+  if (!found) {
+    document.getElementById("auth_label").innerText = "Sorry, you are not authorized to join...";
+  } else {
+    document.getElementById("auth_label").innerText = "Welcome " + fname + ", enter your choices below: ";
+    document.getElementById("bracket").style.visibility = "visible";
+  }
 }
