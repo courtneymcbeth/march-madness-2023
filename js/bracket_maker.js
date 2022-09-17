077b382a5f0026fd9283c32cdfd9b72a0b0359f6
Courtney McBeth <clm325@cornell.edu>
2022-09-17 15:46:23 -0500

    debugging

--- js/bracket_maker.js
+++ js/bracket_maker.js
@@ -94,6 +94,16 @@ function AddListeners() {
     });
   }
 
+  for (let i = 1; i < 3; i++) {
+    children = document.getElementById("west-r3-" + i.toString()).children;
+    children[0].addEventListener("click", function () {
+      setNextOnClick("west-r3-" + i.toString(), 0);
+    });
+    children[1].addEventListener("click", function () {
+      setNextOnClick("west-r3-" + i.toString(), 1);
+    });
+  }
+
   for (let i = 1; i < 9; i++) {
     children = document.getElementById("east-r1-" + i.toString()).children;
     children[0].addEventListener("click", function () {
@@ -111,6 +121,16 @@ function AddListeners() {
     });
     children[1].addEventListener("click", function () {
       setNextOnClick("east-r2-" + i.toString(), 1);
+    });
+  }
+
+  for (let i = 1; i < 3; i++) {
+    children = document.getElementById("east-r3-" + i.toString()).children;
+    children[0].addEventListener("click", function () {
+      setNextOnClick("east-r3-" + i.toString(), 0);
+    });
+    children[1].addEventListener("click", function () {
+      setNextOnClick("east-r3-" + i.toString(), 1);
     });
   }
 
@@ -134,6 +154,16 @@ function AddListeners() {
     });
   }
 
+  for (let i = 1; i < 3; i++) {
+    children = document.getElementById("south-r3-" + i.toString()).children;
+    children[0].addEventListener("click", function () {
+      setNextOnClick("south-r3-" + i.toString(), 0);
+    });
+    children[1].addEventListener("click", function () {
+      setNextOnClick("south-r3-" + i.toString(), 1);
+    });
+  }
+
   for (let i = 1; i < 9; i++) {
     children = document.getElementById("midwest-r1-" + i.toString()).children;
     children[0].addEventListener("click", function () {
@@ -151,6 +181,16 @@ function AddListeners() {
     });
     children[1].addEventListener("click", function () {
       setNextOnClick("midwest-r2-" + i.toString(), 1);
+    });
+  }
+
+  for (let i = 1; i < 3; i++) {
+    children = document.getElementById("midwest-r3-" + i.toString()).children;
+    children[0].addEventListener("click", function () {
+      setNextOnClick("midwest-r3-" + i.toString(), 0);
+    });
+    children[1].addEventListener("click", function () {
+      setNextOnClick("midwest-r3-" + i.toString(), 1);
     });
   }
 }
@@ -173,6 +213,13 @@ function setNextOnClick(id, pos) {
     var newNum = Math.ceil(num / 2);
     var newPos = (num + 1) % 2;
     var newId = idSplit[0] + "-r3-" + newNum.toString();
+    var children = document.getElementById(newId).children;
+    children[newPos].innerText = document.getElementById(id).children[pos].innerText;
+  } else if (idSplit[1] === "r3") {
+    var num = parseInt(idSplit[2]);
+    var newNum = Math.ceil(num / 2);
+    var newPos = (num + 1) % 2;
+    var newId = idSplit[0] + "-r4-" + newNum.toString();
     var children = document.getElementById(newId).children;
     children[newPos].innerText = document.getElementById(id).children[pos].innerText;
   }
