f1cd61ed2440a344fb5b51ea2c14c6ea8a1a5e3e
Courtney McBeth <clm325@cornell.edu>
2022-09-24 15:01:47 -0500

    debugging

--- js/bracket_display.js
+++ js/bracket_display.js
@@ -1,3 +1,5 @@
+var gold = {};
+
 /**
  * Decodes the url to get the requested parameter (for this page, 'auth')
  * @param {string} variable - the variable name
@@ -16,7 +18,19 @@ function getQueryVariable(variable) {
   }
 }
 
-function loadBracket() {
+function readTextFile(file, callback) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType("application/json");
+  rawFile.open("GET", file, true);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4 && rawFile.status == "200") {
+      callback(rawFile.responseText);
+    }
+  }
+  rawFile.send(null);
+}
+
+async function loadBracket() {
   var num = getQueryVariable('number');
 
   let xhr = new XMLHttpRequest();
@@ -33,151 +47,178 @@ function loadBracket() {
 
     setTeams(JSON.parse(ret_data.body));
   };
+
+  await readTextFile("gold_bracket.json", function (text) {
+    gold = JSON.parse(text);
+  });
   xhr.send();
 }
 
 function setTeams(data) {
   var children = [];
+  var regions = ["east", "west", "south", "midwest"];
 
-  var idx = 0;
-  for (let i = 1; i < 9; i++) {
-    children = document.getElementById("west-r1-" + i.toString()).children;
-    children[0].innerText = data.west.first[idx];
-    children[1].innerText = data.west.first[idx + 1];
-    idx = idx + 2;
-  }
+  for (let j = 0; j < regions.length; j++) {
+    // round 1
+    var idx = 0;
+    for (let i = 1; i < 9; i++) {
+      children = document.getElementById(regions[j] + "-r1-" + i.toString()).children;
+      children[0].innerText = data[region[j]]["first"][idx];
+      if (gold[region[j]]["first"][idx].trim().length > 0) {
+        if (data[region[j]]["first"][idx] === gold[region[j]]["first"][idx]) {
+          children[0].classList.add("correct");
+        } else {
+          children[0].classList.add("wrong");
+        }
+      }
 
-  var idx = 0;
-  for (let i = 1; i < 5; i++) {
-    children = document.getElementById("west-r2-" + i.toString()).children;
-    children[0].innerText = data.west.second[idx];
-    children[1].innerText = data.west.second[idx + 1];
-    idx = idx + 2;
-  }
+      children[1].innerText = data[region[j]]["first"][idx + 1];
+      if (gold[region[j]]["first"][idx + 1].trim().length > 0) {
+        if (data[region[j]]["first"][idx + 1] === gold[region[j]]["first"][idx + 1]) {
+          children[1].classList.add("correct");
+        } else {
+          children[1].classList.add("wrong");
+        }
+      }
+      idx = idx + 2;
+    }
 
-  var idx = 0;
-  for (let i = 1; i < 3; i++) {
-    children = document.getElementById("west-r3-" + i.toString()).children;
-    children[0].innerText = data.west.sweet16[idx];
-    children[1].innerText = data.west.sweet16[idx + 1];
-    idx = idx + 2;
-  }
+    // round 2
+    var idx = 0;
+    for (let i = 1; i < 5; i++) {
+      children = document.getElementById(regions[j] + "-r2-" + i.toString()).children;
+      children[0].innerText = data[region[j]]["second"][idx];
+      if (gold[region[j]]["second"][idx].trim().length > 0) {
+        if (data[region[j]]["second"][idx] === gold[region[j]]["second"][idx]) {
+          children[0].classList.add("correct");
+        } else {
+          children[0].classList.add("wrong");
+        }
+      }
 
-  var idx = 0;
-  for (let i = 1; i < 2; i++) {
-    children = document.getElementById("west-r4").children;
-    children[0].innerText = data.west.elite8[idx];
-    children[1].innerText = data.west.elite8[idx + 1];
-    idx = idx + 2;
-  }
+      children[1].innerText = data[region[j]]["second"][idx + 1];
+      if (gold[region[j]]["second"][idx + 1].trim().length > 0) {
+        if (data[region[j]]["second"][idx + 1] === gold[region[j]]["second"][idx + 1]) {
+          children[1].classList.add("correct");
+        } else {
+          children[1].classList.add("wrong");
+        }
+      }
+      idx = idx + 2;
+    }
 
-  idx = 0;
-  for (let i = 1; i < 9; i++) {
-    children = document.getElementById("east-r1-" + i.toString()).children;
-    children[0].innerText = data.east.first[idx];
-    children[1].innerText = data.east.first[idx + 1];
-    idx = idx + 2;
-  }
+    // round 3
+    var idx = 0;
+    for (let i = 1; i < 3; i++) {
+      children = document.getElementById(regions[j] + "-r3-" + i.toString()).children;
+      children[0].innerText = data[region[j]]["sweet16"][idx];
+      if (gold[region[j]]["sweet16"][idx].trim().length > 0) {
+        if (data[region[j]]["sweet16"][idx] === gold[region[j]]["sweet16"][idx]) {
+          children[0].classList.add("correct");
+        } else {
+          children[0].classList.add("wrong");
+        }
+      }
 
-  idx = 0;
-  for (let i = 1; i < 5; i++) {
-    children = document.getElementById("east-r2-" + i.toString()).children;
-    children[0].innerText = data.east.second[idx];
-    children[1].innerText = data.east.second[idx + 1];
-    idx = idx + 2;
-  }
+      children[1].innerText = data[region[j]]["sweet16"][idx + 1];
+      if (gold[region[j]]["sweet16"][idx + 1].trim().length > 0) {
+        if (data[region[j]]["sweet16"][idx + 1] === gold[region[j]]["sweet16"][idx + 1]) {
+          children[1].classList.add("correct");
+        } else {
+          children[1].classList.add("wrong");
+        }
+      }
+      idx = idx + 2;
+    }
 
-  idx = 0;
-  for (let i = 1; i < 3; i++) {
-    children = document.getElementById("east-r3-" + i.toString()).children;
-    children[0].innerText = data.east.sweet16[idx];
-    children[1].innerText = data.east.sweet16[idx + 1];
-    idx = idx + 2;
-  }
+    // round 4
+    var idx = 0;
+    for (let i = 1; i < 2; i++) {
+      children = document.getElementById(regions[j] + "-r4").children;
+      children[0].innerText = data[region[j]]["elite8"][idx];
+      if (gold[region[j]]["elite8"][idx].trim().length > 0) {
+        if (data[region[j]]["elite8"][idx] === gold[region[j]]["elite8"][idx]) {
+          children[0].classList.add("correct");
+        } else {
+          children[0].classList.add("wrong");
+        }
+      }
 
-  idx = 0;
-  for (let i = 1; i < 2; i++) {
-    children = document.getElementById("east-r4").children;
-    children[0].innerText = data.east.elite8[idx];
-    children[1].innerText = data.east.elite8[idx + 1];
-    idx = idx + 2;
-  }
-
-  idx = 0;
-  for (let i = 1; i < 9; i++) {
-    children = document.getElementById("south-r1-" + i.toString()).children;
-    children[0].innerText = data.south.first[idx];
-    children[1].innerText = data.south.first[idx + 1];
-    idx = idx + 2;
-  }
-
-  idx = 0;
-  for (let i = 1; i < 5; i++) {
-    children = document.getElementById("south-r2-" + i.toString()).children;
-    children[0].innerText = data.south.second[idx];
-    children[1].innerText = data.south.second[idx + 1];
-    idx = idx + 2;
-  }
-
-  idx = 0;
-  for (let i = 1; i < 3; i++) {
-    children = document.getElementById("south-r3-" + i.toString()).children;
-    children[0].innerText = data.south.sweet16[idx];
-    children[1].innerText = data.south.sweet16[idx + 1];
-    idx = idx + 2;
-  }
-
-  idx = 0;
-  for (let i = 1; i < 2; i++) {
-    children = document.getElementById("south-r4").children;
-    children[0].innerText = data.south.elite8[idx];
-    children[1].innerText = data.south.elite8[idx + 1];
-    idx = idx + 2;
-  }
-
-  idx = 0;
-  for (let i = 1; i < 9; i++) {
-    children = document.getElementById("midwest-r1-" + i.toString()).children;
-    children[0].innerText = data.midwest.first[idx];
-    children[1].innerText = data.midwest.first[idx + 1];
-    idx = idx + 2;
-  }
-
-  idx = 0;
-  for (let i = 1; i < 5; i++) {
-    children = document.getElementById("midwest-r2-" + i.toString()).children;
-    children[0].innerText = data.midwest.second[idx];
-    children[1].innerText = data.midwest.second[idx + 1];
-    idx = idx + 2;
-  }
-
-  idx = 0;
-  for (let i = 1; i < 3; i++) {
-    children = document.getElementById("midwest-r3-" + i.toString()).children;
-    children[0].innerText = data.midwest.sweet16[idx];
-    children[1].innerText = data.midwest.sweet16[idx + 1];
-    idx = idx + 2;
-  }
-
-  idx = 0;
-  for (let i = 1; i < 2; i++) {
-    children = document.getElementById("midwest-r4").children;
-    children[0].innerText = data.midwest.elite8[idx];
-    children[1].innerText = data.midwest.elite8[idx + 1];
-    idx = idx + 2;
+      children[1].innerText = data[region[j]]["elite8"][idx + 1];
+      if (gold[region[j]]["elite8"][idx + 1].trim().length > 0) {
+        if (data[region[j]]["elite8"][idx + 1] === gold[region[j]]["elite8"][idx + 1]) {
+          children[1].classList.add("correct");
+        } else {
+          children[1].classList.add("wrong");
+        }
+      }
+      idx = idx + 2;
+    }
   }
 
   children = document.getElementById("f4-east-west").children;
   children[0].innerText = data.final4.west;
+  if (gold["final4"]["west"].trim().length > 0) {
+    if (data["final4"]["west"] === gold["final4"]["west"]) {
+      children[0].classList.add("correct");
+    } else {
+      children[0].classList.add("wrong");
+    }
+  }
+
   children[1].innerText = data.final4.east;
+  if (gold["final4"]["east"].trim().length > 0) {
+    if (data["final4"]["east"] === gold["final4"]["east"]) {
+      children[1].classList.add("correct");
+    } else {
+      children[1].classList.add("wrong");
+    }
+  }
 
   children = document.getElementById("f4-south-midwest").children;
   children[0].innerText = data.final4.south;
+  if (gold["final4"]["south"].trim().length > 0) {
+    if (data["final4"]["south"] === gold["final4"]["south"]) {
+      children[0].classList.add("correct");
+    } else {
+      children[0].classList.add("wrong");
+    }
+  }
+
   children[1].innerText = data.final4.midwest;
+  if (gold["final4"]["midwest"].trim().length > 0) {
+    if (data["final4"]["midwest"] === gold["final4"]["midwest"]) {
+      children[1].classList.add("correct");
+    } else {
+      children[1].classList.add("wrong");
+    }
+  }
 
   children = document.getElementById("f4-championship").children;
   children[0].innerText = data["championship"]["east-west"];
+  if (gold["championship"]["east-west"].trim().length > 0) {
+    if (data["championship"]["east-west"] === gold["championship"]["east-west"]) {
+      children[0].classList.add("correct");
+    } else {
+      children[0].classList.add("wrong");
+    }
+  }
+
   children[1].innerText = data["championship"]["south-midwest"];
+  if (gold["championship"]["south-midwest"].trim().length > 0) {
+    if (data["championship"]["south-midwest"] === gold["championship"]["south-midwest"]) {
+      children[0].classList.add("correct");
+    } else {
+      children[0].classList.add("wrong");
+    }
+  }
 
   document.getElementById("f4-winner").innerText = "Winner: " + data["championship"]["winner"];
+  if (gold["championship"]["winner"].trim().length > 0) {
+    if (data["championship"]["winner"] === gold["championship"]["winner"]) {
+      document.getElementById("f4-winner").classList.add("correct");
+    } else {
+      document.getElementById("f4-winner").classList.add("wrong");
+    }
+  }
 }
